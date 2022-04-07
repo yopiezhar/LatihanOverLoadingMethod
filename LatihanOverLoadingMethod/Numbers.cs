b41ee80cb2234e43500663bfa377f528cc1b341a
yopiezhar <yopiezhar@students.amikom.ac.id>
2022-04-07 13:10:46 +0700

    Add project files.

---
+++ LatihanOverLoadingMethod/Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LatihanOverLoadingMethod
+{
+    internal class Numbers
+    {
+        public int FindingMinimum(int number1, int number2)
+        {
+            return (int)Math.Min(number1, number2);
+
+        }
+
+        public int FindingMinimum(int number1, int number2, float number3)
+        {
+            return (int)Math.Min(number1, Math.Min(number2, number3));
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return (int)Math.Max(number1, number2);
+
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            return (int)Math.Max(number1, Math.Max(number2, number3));
+        }
+    }
+}
